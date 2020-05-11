1d461162d9e0f8fd78e8d8d6c375d1c47880f7e6
Shayan Mohsin <shaymotionfilms@gmail.com>
2020-05-11 14:54:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const num1 = 2
-const num2 = 31
-const multiply = num1 * num2
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
 
 
-const random = Math.ceil(Math.random() * 10)
-
-
-
-const mod = 14 % 5
+const random = Math.ceil(Math.random() * 10);
 
 
 
-const max = Math.max(16, 17, 18, 19, 20)
+const mod = 14 % 5;
+
+
+
+const max = Math.max(16, 17, 18, 19, 20);
